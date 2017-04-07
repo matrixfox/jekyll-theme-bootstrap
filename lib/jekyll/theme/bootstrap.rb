30749798109c4be5e339c18bbd355ade120865b3
matrixfox <matrixfox@gmail.com>
2017-04-06 19:01:41 -0700

    bundle gem

---
+++ lib/jekyll/theme/bootstrap.rb
@@ -0,0 +1,9 @@
+require "jekyll/theme/bootstrap/version"
+
+module Jekyll
+  module Theme
+    module Bootstrap
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/theme/bootstrap/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Theme
+    module Bootstrap
+      VERSION = "0.1.0"
+    end
+  end
+end
